73bd4fbbcd86f59a2eb356fc28c37d3a017c53be
shenjia <jshenabc@gmail.com>
2018-08-19 00:32:28 -0400

    add dashboard toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap';
+import Dashboard from './Dashboard'
 
 class App extends Component {
   render() {
     return (
       <div>
-        test
+        <Dashboard />
       </div>
     )
   }
