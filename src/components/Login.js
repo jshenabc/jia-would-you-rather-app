9ae811b0cab6d50af2487d1c3bb20733e8ae0622
shenjia <jshenabc@gmail.com>
2018-08-18 21:08:08 -0400

    Initial commit to create app template

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap';
+
+class Login extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Login
